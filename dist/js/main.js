e1004dfc2c023dcff2712660ca4f9deef82f33f8
Paul <hqdrone@gmail.com>
2021-07-05 16:54:32 +0300

    upd

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
+const App = {
+	data() {
+		return {
+			cards: []
+		}
+	},
+	methods: {
+		async getCards() {
+			const {data} = await axios.get('https://6075786f0baf7c0017fa64ce.mockapi.io/products')
+			this.cards = data
+		},
+		getDate(val){
+			return moment(new Date(val * 1000)).format('DD.MM.YY, hh:mm');
+		}
+	},
+	mounted() {
+		this.getCards()
+	}
+}
 
-	// ...
-
-})
+Vue.createApp(App).mount('#app')
