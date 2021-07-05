0f0a83ba7cfd1bcfc3dfa66539d6b7034d2fa357
Paul <hqdrone@gmail.com>
2021-07-06 00:22:28 +0300

    upd

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,24 @@
 const App = {
 	data() {
 		return {
-			cards: []
+			cards: [],
+			page: 1,
+			limit: 16,
+			loading: false
 		}
 	},
 	methods: {
 		async getCards() {
-			const {data} = await axios.get('https://6075786f0baf7c0017fa64ce.mockapi.io/products')
-			this.cards = data
-		},
+      try {
+      	this.loading = true
+        const {data} = await axios.get(`https://6075786f0baf7c0017fa64ce.mockapi.io/products?page=${this.page}&limit=${this.limit}`)
+        this.cards.push(...data)
+				this.page++
+				this.loading = false
+      } catch (e) {
+        console.log(e.message)
+      }
+    },
 		getDate(val){
 			return moment(new Date(val * 1000)).format('DD.MM.YY, hh:mm');
 		}
